23e75da3c3ccc6604ab3e8aca4da462ca3a6ffdb
nickncb96 <nicholasncb12@hotmail.com>
2024-02-19 12:43:42 +0000

    user input prompts and fie writing functionality

--- starter/index.js
+++ starter/index.js
@@ -1,21 +1,80 @@
+
+// Import: node.js modules and exteral packages 
 const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+// Questions array for user input
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Provide installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide usage information:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache", "GPL"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Provide contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide test instructions:",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
 ];
 
-// function to write README file
+// Function to write the README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("README.md successfully generated!");
+    }
+  });
 }
 
-// function to initialize program
+// Function to initialisee program
 function init() {
-
+  // Prompt the user for input based on the array questions
+  inquirer.prompt(questions).then((answers) => {
+    // Generate markdown content based on user input
+    const markdown = generateMarkdown(answers);
+    // Write the generated markdown content to README.md file
+    writeToFile("README.md", markdown);
+  });
 }
 
-// function call to initialize program
+//  init function begin the program
 init();
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,44 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// utils/generateMarkdown.js
 
-`;
+function generateMarkdown(data) {
+  let markdown = `# ${data.title}\n\n`;
+
+  // Description section
+  markdown += `## Description\n${data.description}\n\n`;
+
+  // Table of Contents
+  markdown += '## Table of Contents\n';
+  if (data.installation) markdown += `1. [Installation](#installation)\n`;
+  if (data.usage) markdown += `2. [Usage](#usage)\n`;
+  if (data.license) markdown += `3. [License](#license)\n`;
+  if (data.contributing) markdown += `4. [Contributing](#contributing)\n`;
+  if (data.tests) markdown += `5. [Tests](#tests)\n`;
+  markdown += `6. [Questions](#questions)\n\n`;
+
+  // Installation section
+  if (data.installation) markdown += `## Installation\n${data.installation}\n\n`;
+
+  // Usage section
+  if (data.usage) markdown += `## Usage\n${data.usage}\n\n`;
+
+  // License section
+  if (data.license) {
+    markdown += `## License\n`;
+    markdown += `[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})\n\n`;
+    markdown += `This project is licensed under the ${data.license} license.\n\n`;
+  }
+
+  // Contributing section
+  if (data.contributing) markdown += `## Contributing\n${data.contributing}\n\n`;
+
+  // Tests section
+  if (data.tests) markdown += `## Tests\n${data.tests}\n\n`;
+
+  // Questions section
+  markdown += `## Questions\n`;
+  markdown += `For questions about the project, please contact [${data.username}](https://github.com/${data.username}) via email at ${data.email}.\n\n`;
+
+  return markdown;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown
